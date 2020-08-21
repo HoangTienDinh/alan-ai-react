b83e2931a9ee205d507ccaeee20d57074d6195d4
Hoang Dinh <hoangdinh90@gmail.com>
2020-08-20 19:53:06 -0700

    add .env to gitignore and import alan-ai

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
+import alanBtn from "@alan-ai/alan-sdk-web";
+
+const alanKey = "";
 
 const App = () => {
+    
+  useEffect(() => {
+    alanBtn({
+      key: alanKey,
+    });
+  }, []);
+
   return (
     <div>
       <h1>Alan AI News Application</h1>
