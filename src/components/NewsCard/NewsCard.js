b968e586b4eedce9fc056be9356ff45dcda5d0b1
Hoang Dinh <hoangdinh90@gmail.com>
2020-08-20 22:09:47 -0700

    fill out NewsCard component with MUI elements

--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -1,11 +1,47 @@
-import React from 'react'
+import React from "react";
+import {
+  Card,
+  CardActions,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Button,
+  Typography,
+} from "@material-ui/core";
 
 const NewsCard = ({ article }) => {
-    return (
+  return (
+    <Card>
+      <CardActionArea>
+        <CardMedia />
         <div>
-            News Card
+          <Typography
+            variant="body2"
+            color="textSecondary"
+            component="h2"
+          ></Typography>
+          <Typography
+            variant="body2"
+            color="textSecondary"
+            component="h2"
+          ></Typography>
         </div>
-    )
-}
+        <Typography gutterButtom variant="h5"></Typography>
+        <CardContent>
+          <Typography
+            varaint="body2"
+            color="textSecondard"
+            component="p"
+          ></Typography>
+        </CardContent>
+      </CardActionArea>
 
-export default NewsCard
+      <CardActions>
+        <Button size="small" color="primary"></Button>
+        <Typography variant="h5" color="textSecondary"></Typography>
+      </CardActions>
+    </Card>
+  );
+};
+
+export default NewsCard;
