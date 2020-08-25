43b871d2eff90ba78c2ef2fd293eb7a8fe91ef14
Hoang Dinh <hoangdinh90@gmail.com>
2020-08-24 23:31:20 -0700

    add logo and styles to App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from "react";
 import alanBtn from "@alan-ai/alan-sdk-web";
 import NewsCards from "./components/NewsCards/NewsCards";
+import useStyles from "./styles";
 
 const alanKey = process.env.REACT_APP_ALAN_KEY;
 
 const App = () => {
   const [newsArticles, setNewsArticles] = useState([]);
+  const classes = useStyles();
 
   useEffect(() => {
     alanBtn({
@@ -20,8 +22,14 @@ const App = () => {
 
   return (
     <div>
-      <h1>Alan AI News Application</h1>
-      <NewsCards articles={newsArticles}/>
+      <div className={classes.logoContainer}>
+        <img
+          src="https://alan.app/voice/images/previews/preview.jpg"
+          className={classes.alanLogo}
+          alt="Alan Logo"
+        />
+      </div>
+      <NewsCards articles={newsArticles} />
     </div>
   );
 };
