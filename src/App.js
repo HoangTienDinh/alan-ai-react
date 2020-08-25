8b560772bd5bb925cfd6da99757ac3d81b4666f8
Hoang Dinh <hoangdinh90@gmail.com>
2020-08-25 16:39:17 -0700

    use words-to-number to parse speech, implement alanBtn() to play text

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import alanBtn from "@alan-ai/alan-sdk-web";
 import NewsCards from "./components/NewsCards/NewsCards";
 import useStyles from "./styles";
+import wordsToNumbers from "words-to-numbers";
 
 const alanKey = process.env.REACT_APP_ALAN_KEY;
 
@@ -13,15 +14,27 @@ const App = () => {
   useEffect(() => {
     alanBtn({
       key: alanKey,
-      onCommand: ({ command, articles, number}) => {
+      onCommand: ({ command, articles, number }) => {
         if (command === "newHeadlines") {
           setNewsArticles(articles);
           // resets highlight when new search is being done
-          setActiveArticle(-1)
-        } else if (command === 'highlight') {
-          setActiveArticle((prevActiveArticle) => prevActiveArticle + 1)
-        } else if (command === 'open') {
-          window.open(articles[number].url, '_blank');
+          setActiveArticle(-1);
+        } else if (command === "highlight") {
+          setActiveArticle((prevActiveArticle) => prevActiveArticle + 1);
+        } else if (command === "open") {
+          // fuzzy allows for parsing of numbers such as four (for)
+          const parsedNumber =
+            number.length > 2
+              ? wordsToNumbers(number, { fuzzy: true })
+              : number;
+          const article = articles[parsedNumber - 1];
+
+          if (parsedNumber > 20) {
+            alanBtn().playText("Please try that again.");
+          } else if (article) {
+            window.open(article.url, "_blank");
+            alanBtn().playText("Opening...");
+          }
         }
       },
     });
