505a0444a64e1e0d46ec8aa415a5406fad32e35c
Hoang Dinh <hoangdinh90@gmail.com>
2020-09-09 10:16:33 -0700

    change website title

--- src/App.js
+++ src/App.js
@@ -48,7 +48,6 @@ const App = () => {
           className={classes.alanLogo}
           alt="Alan Logo"
         />
-        HELLO
       </div>
       <NewsCards articles={newsArticles} activeArticle={activeArticle} />
     </div>
